781d3a9bcb2c8039f330a868b99e973e52b7c72a
marisela <mariselacerda@msn.com>
2017-03-19 09:24:17 -0700

    First Check In of Cerda Bank Project

---
+++ CerdaBank/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CerdaBank
+{
+    class Account
+    {
+        #region Properties
+        /// <summary>
+        /// Create account number as a public integer
+        /// </summary>
+        public int AccountNumber { get;} 
+        public string EmailAddress { get; set; }
+        public decimal Balance { get; private set;}
+        public string AccountType { get; set; } 
+        //Type of Account
+        #endregion Properties
+
+        #region Methods
+        public void Deposit(decimal amount)
+        {
+            //Balance = Balance + amount
+            Balance += amount;
+        }
+
+        public void withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+        #endregion
+
+    }
+}
---
+++ CerdaBank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CerdaBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account1 = new Account();
+            account1.EmailAddress = "test@test.com";
+            account1.AccountType = "Checking";
+            account1.Deposit(100.01M);
+            Console.WriteLine($"AccountNumber:{account1.AccountNumber},TypeOfAccount: {account1.AccountType},EmailAddress: {account1.EmailAddress}, Balance: {account1.Balance:C}");
+        }
+
+    }
+}
