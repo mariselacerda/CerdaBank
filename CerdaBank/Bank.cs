472b81ff0e20b7d7533abba0bb2f7c887906dc76
marisela <mariselacerda@msn.com>
2017-04-03 09:28:37 -0700

    updating for 3.26 class

--- CerdaBank/Bank.cs
+++ CerdaBank/Bank.cs
@@ -8,12 +8,27 @@ namespace CerdaBank
 {
     static class Bank
     {
+        private static List<Account> accounts = new List<Account>();
         public static Account CreateAccount(string emailAddress, AccountTypes typeOfAccount, decimal amount) 
         {
            var account = new Account { EmailAddress = emailAddress, AccountType = typeOfAccount };
            account.Deposit(amount);
+           accounts.Add(account);
            return account; 
 
         }
-     }
+
+        public static List<Account> GetAllAccounts() { 
+            return accounts;
+        }
+
+        public static void Deposit(int accountNumber, decimal amount)
+        {
+            var account = accounts.Where(a => a.AccountNumber == accountNumber).FirstOrDefault(); 
+            if (account != null)
+            {
+                account.Deposit(amount);
+            }
+        }
+    }
 }
--- CerdaBank/Program.cs
+++ CerdaBank/Program.cs
@@ -12,38 +12,66 @@ namespace CerdaBank
         {
 
             Console.WriteLine("***********Welcome to Cerda Bank*************");
-            Console.WriteLine("Please select from the following options");
-            Console.WriteLine("0. Exit");
-            Console.WriteLine("1: Create a new account");
-            Console.WriteLine("2: Deposit");
-            Console.WriteLine("3. Withdraw");
-            var optioninput = Console.ReadLine();
-            switch (optioninput)
-            {
-                case "0":
-                    Console.WriteLine("Thank You for Visiting Goodbye");
-                    break;
-                case "1":
-                    //var account1 = new Account();
-                    Console.Write("Please provide your email address: ");
-                    var emailAddress = Console.ReadLine();
-                    Console.Write("Please provide the type of account: ");
-                    var typeofAccount = Console.ReadLine();
-                    var account1 = Bank.CreateAccount(emailAddress, AccountTypes.Checking, 0.0M);
 
-                    //var account1 = new Account { EmailAddress = emailAddress, AccountType = typeofAccount };
-                    Console.WriteLine($"AccountNumber:{account1.AccountNumber},TypeOfAccount: {account1.AccountType},EmailAddress: {account1.EmailAddress}, Balance: {account1.Balance:C}");
-                    break;
-                case "2":
-                    break;
-                case "3":
-                    break;
-                default:
-                    break;
+            while (true)
+            {
+                Console.WriteLine("Please select from the following options");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1: Create a new account");
+                Console.WriteLine("2: Deposit");
+                Console.WriteLine("3. Withdraw");
+                Console.WriteLine("4. Print all accounts");
+                var optioninput = Console.ReadLine();
+                switch (optioninput)
+                {
+                    case "0":
+                        Console.WriteLine("Thank You for Visiting Goodbye");
+                        return;
+                    case "1":
+                        //var account1 = new Account();
+                        Console.WriteLine("Please provide your email address: ");
+                        var emailAddress = Console.ReadLine();
+                        Console.WriteLine("Please provide the type of account: ");
+                        var accountTypes = Enum.GetNames(typeof(AccountTypes));
+                        for (var i = 0; i < accountTypes.Length; i++)
+                        {
+                            Console.WriteLine($"{i + 1}. {accountTypes[i]}");
+                        }
+                        var typeofAccount = Convert.ToInt32(Console.ReadLine());
+                        
+                        var account1 = Bank.CreateAccount(emailAddress, (AccountTypes)(typeofAccount-1), 0.0M);
+
+                        //var account1 = new Account { EmailAddress = emailAddress, AccountType = typeofAccount };
+                        Console.WriteLine($"AccountNumber:{account1.AccountNumber},TypeOfAccount: {account1.AccountType},EmailAddress: {account1.EmailAddress}, Balance: {account1.Balance:C}");
+                        break;
+                    case "2":
+                        PrintAllAccounts();
+                        Console.WriteLine("Pick an account number to deposit: ");
+                        var accountNumber = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Amount to Deposit: ");
+                        var amount = Convert.ToDecimal(Console.ReadLine());
+                        Bank.Deposit(accountNumber, amount);
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        PrintAllAccounts();
+                        break;
+                    default:
+                        break;
+                }
             }
-           
-            
+
+
         }
 
+        private static void PrintAllAccounts()
+        {
+            var accounts = Bank.GetAllAccounts();
+            foreach (var account in accounts)
+            {
+                Console.WriteLine($"AccountNumber:{account.AccountNumber},TypeOfAccount: {account.AccountType},EmailAddress: {account.EmailAddress}, Balance: {account.Balance:C}");
+            }
+        }
     }
 }
